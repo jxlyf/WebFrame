680d05c323fa1502e621ccc6d183f1243d525359
lyf <jx.lyf@qq.com>
2019-10-08 15:09:58 +0800

    兼容ie

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import 'babel-polyfill';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import iView from 'iview';
